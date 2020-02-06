463db0d8edfb0a4d83e63a8fd77041301df398b1
Omar Acosta <omardil.acosta@gmail.com>
2020-02-06 17:00:04 -0600

    Update ACT06 Spaceship activity

---
+++ MOD04_Modificadores_de_acceso/02 Actividad personal/src/spaceship/Spaceship.java
@@ -0,0 +1,67 @@
+package spaceship;
+
+public class Spaceship {
+	
+	private String shipName;
+	private int passengerCount;
+	private int maxPassengers;
+	
+	public Spaceship(String shipName, int maxPassengers) {
+		this.shipName = shipName;
+		this.maxPassengers = (maxPassengers >= 0)?maxPassengers:100; 
+		/* Code above is equal to: 
+		if (maxPassengers >= 0)
+			this.maxPassengers = maxPassengers; 
+		else
+			this.maxPassengers = 100;
+		*/
+	}
+	
+	public void setShipName(String shipName) {
+		this.shipName = shipName;
+	}
+	
+	public String getShipName() {
+		return this.shipName;
+	}
+	
+	public int getMaxPassengers() {
+		return this.maxPassengers;
+	}
+	
+	public void board(int boardingPassengers) {
+		if ( this.passengerCount + boardingPassengers <= maxPassengers) {
+			this.passengerCount += boardingPassengers;
+		}
+	} 
+	
+	public void leave(int leavingPassengers) {
+		if ( this.passengerCount - leavingPassengers >= 0 ) {
+			this.passengerCount -= leavingPassengers;
+		}
+	}
+	
+	public int getPassengerCount() {
+		return this.passengerCount;
+	}
+	
+	public void transferTo(Spaceship newShip) {
+		if (newShip == null)
+			return;
+	
+		if (this.getPassengerCount() + newShip.getPassengerCount() <= newShip.getMaxPassengers()) {
+			newShip.board(this.getPassengerCount());
+			this.leave(this.getPassengerCount());
+		}
+	}
+	
+	// Method toString() will automatically be called when invoked 
+	// inside a System.out.println() instruction.
+	public String toString() {
+		String out = "Ship name: " + this.shipName + "\n" +
+					 "Passenger count: " + this.passengerCount + "\n" + 
+					 "Max passengers: " + this.maxPassengers;
+		return out;
+	}
+	
+}
---
+++ MOD04_Modificadores_de_acceso/02 Actividad personal/src/spaceship/SpaceshipDemo.java
@@ -0,0 +1,65 @@
+package spaceship;
+
+public class SpaceshipDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Problem 1");
+		Spaceship s1 = new Spaceship("Battlecruiser", 1000);
+		s1.board(250);
+		s1.leave(100);
+		s1.board(5);
+		System.out.println(s1);
+		System.out.println("----------");	
+		//----------------------------------------------------
+		
+		System.out.println("Problem 2");		
+		Spaceship Hyperion = new Spaceship("", 2500);
+		Hyperion.setShipName("Hyperion");
+		Hyperion.board(450);
+		Hyperion.board(3655);
+		
+		Spaceship TheMilano = new Spaceship("The Milano", 10);
+		TheMilano.setShipName("Milano");
+		
+		Hyperion = TheMilano;
+		TheMilano.board(1);
+		Hyperion.board(4);
+		
+		System.out.println(TheMilano);
+		System.out.println(Hyperion);
+		System.out.println("----------");		
+		//----------------------------------------------------
+
+		System.out.println("Problem 3");				
+		Spaceship Normandy, Andromeda;
+		Normandy = new Spaceship("Normandy", 30);
+		Andromeda = new Spaceship("Andromeda", 500);
+		Andromeda = Normandy;
+		Normandy.setShipName("SSV Normandy SR-1");
+		Normandy.board(10);
+		Andromeda.board(10);
+		Andromeda.setShipName("Andromeda ISS");
+		Andromeda.board(20);
+		
+		System.out.println(Andromeda);
+		System.out.println(Normandy);		
+		
+		//----------------------------------------------------
+		System.out.println("Problem 4");
+		Spaceship Voyager1, Apollo;
+		Voyager1 = new Spaceship("Voyager 1", 5);
+		Apollo = new Spaceship("Apollo 11", 10);
+		
+		Voyager1.board(3);
+		Voyager1.transferTo(Apollo);
+		
+		Voyager1.board(5);
+		Voyager1.transferTo(Apollo);
+		
+		Voyager1.board(2);
+		Voyager1.transferTo(Apollo);
+		
+	}
+
+}
